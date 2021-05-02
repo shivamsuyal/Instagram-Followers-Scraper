881699c0036a86c8c8226a9a05225e5d9182130b
shivamsuyal <75232486+shivamsuyal@users.noreply.github.com>
2021-05-02 19:38:35 +0530

    Update insta.py

--- insta.py
+++ insta.py
@@ -1,10 +1,18 @@
-import requests,json
+import requests,json,random
 import urllib.parse
 from datetime import datetime
+agent=[
+"Mozilla/5.0 (Linux; Android 8.0.0; SM-G960F Build/R16NW) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.84 Mobile Safari/537.36",
+"Mozilla/5.0 (Linux; Android 6.0; HTC One X10 Build/MRA58K; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/61.0.3163.98 Mobile Safari/537.36",
+"Mozilla/5.0 (iPhone; CPU iPhone OS 12_0 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/12.0 Mobile/15E148 Safari/604.1",
+"Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36",
+"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/36.0.1944.0 Safari/537.36",
+"Mozilla/5.0 (X11; FreeBSD i386) AppleWebKit/535.2 (KHTML, like Gecko) Chrome/15.0.874.121 Safari/535.2"
+]
 
 username=input("Username => ")
 password=input("Password => ")
-
+   
 csrf="csrftoken="
 hash1=""
 
@@ -21,7 +29,7 @@ payload = {
     'optIntoOneTap': 'false'
 }
 login_header = {
-    "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36",    
+    "User-Agent": random.choice(agent),    
     "X-Requested-With": "XMLHttpRequest",
     "Referer": "https://www.instagram.com/accounts/login/",
     "x-csrftoken": csrf
@@ -40,15 +48,10 @@ with requests.Session() as r:
    id_response=r.get(q4)
    json_data = json.loads(id_response.text)
    data_len=json_data["data"]["user"]["edge_follow"]["count"]
-   print(f"(*) Followers \n")
-   for i in range(data_len):
-      u1=json_data["data"]["user"]["edge_follow"]["edges"][i]["node"]["username"]
-      f1=json_data["data"]["user"]["edge_follow"]["edges"][i]["node"]["follows_viewer"]
-      if not f1:
-         not_following.append(u1)
-      print(u1)
-      print(f1)
-      print("")
-   print(f"Not Following \n {not_following}")
-
    
+   for i in range(data_len):
+    u1=json_data["data"]["user"]["edge_follow"]["edges"][i]["node"]["username"]
+    f1=json_data["data"]["user"]["edge_follow"]["edges"][i]["node"]["follows_viewer"]
+    if not f1:
+      not_following.append(u1)
+   print(f"\nFollowing => {data_len}\n\nNot Following \n {not_following}")
